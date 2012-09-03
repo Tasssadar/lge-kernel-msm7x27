0df1fafa310c55caa460749018eb37542a7f46b5
Tassadar <vbocek@gmail.com>
2012-09-03 09:31:31 +0200

    Change compile script a bit

--- compile.sh
+++ compile.sh
@@ -1,7 +1,12 @@
 threads=4
-if [ $# == 1 ] ; then
+param=""
+if [ $# > 0 ] ; then
     threads=$1
 fi
 
+if [ $# > 1 ] ; then
+    param=$2
+fi
+
 export PATH=$PATH:/home/tassadar/arm-eabi-4.4.0/bin
-make ARCH=arm CROSS_COMPILE=arm-eabi- -j$threads
+make ARCH=arm CROSS_COMPILE=arm-eabi- -j$threads $param
